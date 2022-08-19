3d7cd658ad4aac5e3239fcd82be1978dd8b44ab4
BigMacKIM <godminjong@naver.com>
2022-08-20 05:31:54 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 40964 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int num;
+    cin >> num;
+    int DP[10000000];
+    for(int i = 2; i <= num; i++){
+        DP[i] = DP[i-1] +1;
+        if(i %2 == 0)
+            DP[i] = min(DP[i], DP[i/2] +1);
+        if( i%3 ==0)
+            DP[i] = min(DP[i], DP[i/3] +1);
+    }
+    cout << DP[num];
+}
