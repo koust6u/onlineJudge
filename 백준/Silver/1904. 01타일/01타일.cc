708f4a7facc9d77570bffd2b5d51adea845e117b
BigMacKIM <godminjong@naver.com>
2022-08-23 19:41:20 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int DP[1000001];
+
+int main(){
+    int count;
+    cin >> count;
+    DP[0] = 0;
+    DP[1] = 1;
+    DP[2] = 2;
+    for(int i = 3; i <= count; i++){
+        DP[i] = (DP[i-1]+ DP[i-2])%15746;
+    }
+
+    auto temp = DP[count] % 15746;
+    cout << temp;
+}
