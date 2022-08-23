b41d500b02326e014d92a78fffc57c6676fb23bf
BigMacKIM <godminjong@naver.com>
2022-08-24 02:08:19 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 144 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
-void hanoi(int n, int start, int to, int bypass)
-{
-    if(n == 1)
-        printf("%d %d\n", start, to);
-    else
-    {
-        hanoi(n-1,start,bypass,to);
-        printf("%d %d\n",start,to);
-        hanoi(n-1,bypass,to,start);
+
+
+void hanoi(int from, int to, int number){
+    if(number == 1){
+        printf("%d %d\n", from, to);
+        return;
     }
+        int empty = 6 - from - to;
+        hanoi(from, empty, number-1);
+        printf("%d %d\n", from, to);
+        hanoi(empty, to,number-1);
 }
-int main() {
-    int num;
-    cin >> num;
-    cout << (1<<num) -1 << "\n";
-    hanoi(num,1,3,2);
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int numberOfDisk;
+    cin >> numberOfDisk;
+    cout << (1<<numberOfDisk)-1<< endl;
+    hanoi(1,3,numberOfDisk);
 }
