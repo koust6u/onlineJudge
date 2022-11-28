c2084d98a58be628238450d42da2567ff601568d
김민종 <godminjong@naver.com>
2022-11-28 18:51:51 +0900

    [Silver II] Title: 제곱수의 합, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int DP[100001];
+
+int main(){
+    int N;
+    cin >> N;
+    for(int i =0 ; i <= N; i++){
+        DP[i] = i;
+        for(int j = 1;  j*j <= i;j++){
+            DP[i] = min(DP[i], DP[i-j*j]+1);
+        }
+    }
+
+    cout << DP[N];
+}
