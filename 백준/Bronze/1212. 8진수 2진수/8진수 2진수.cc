b6d9552b53afafd3e4cbfd4cbe7ae3f4fed69a6f
김민종 <godminjong@naver.com>
2023-02-14 22:21:42 +0900

    [Bronze II] Title: 8진수 2진수, Time: 12 ms, Memory: 5548 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.cc
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.cc
@@ -22,7 +22,7 @@ map<char,string> makeMap(){
 }
 
 int main(){
-    ios_base :: sync_with_stdio(true);
+    ios_base :: sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     bool flag = true;
